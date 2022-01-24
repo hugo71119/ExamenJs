14a7b27487114686c111b6b2020e5b417016d102
Victor Hugo Sánchez <hugo71119@gmail.com>
2022-01-24 15:30:05 -0600

    1er commit, variables

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+let nombre = 'Victor';
+let apellido = 'Sánchez';
+let usuario_platzi = 'hugo71119';
+let edad = 20;
+let email = 'hugo71119@gmail.com';
+let mayor_edad = true;
+let dinero_ahorrado = 289;
+let deudas = 89;
+
+let fullName = `Nombre: ${nombre} ${apellido}`;
+let TotalAhorrado = `Dinero total ahorrado sin deudas: ${(dinero_ahorrado-deudas)}`;
+
+
+
+console.log(fullName);
+console.log(TotalAhorrado);
