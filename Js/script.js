3c30bc328584f3357c2aa01317ac0d514e7e51e9
Victor Hugo Sánchez <hugo71119@gmail.com>
2022-01-24 15:30:52 -0600

    2do commit, funciones

--- Js/script.js
+++ Js/script.js
@@ -10,7 +10,17 @@ let deudas = 89;
 let fullName = `Nombre: ${nombre} ${apellido}`;
 let TotalAhorrado = `Dinero total ahorrado sin deudas: ${(dinero_ahorrado-deudas)}`;
 
+//Funciones
+function user(nombre, apellidos, nickname){
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.nickname = nickname;
+};
 
+let nuevoUsuario = new user('Carlos', 'Balderas Gonzalez', 'carlos65g');
+let printNUser = `Nombre del nuevo usuario: ${nuevoUsuario.nombre} ${nuevoUsuario.apellidos} con nickname '${nuevoUsuario.nickname}'`;
 
 console.log(fullName);
 console.log(TotalAhorrado);
+console.log(nuevoUsuario);
+console.log(printNUser);
