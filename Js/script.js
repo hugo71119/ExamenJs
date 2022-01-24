6f489205e91bf38b2ede06ad6ae2bfb7b4a6777b
Victor Hugo Sánchez <hugo71119@gmail.com>
2022-01-24 17:05:39 -0600

    3er commit, Condicionales

--- Js/script.js
+++ Js/script.js
@@ -24,3 +24,19 @@ console.log(fullName);
 console.log(TotalAhorrado);
 console.log(nuevoUsuario);
 console.log(printNUser);
+
+//Condicionales
+const tipoDeSuscripcion = "sad";
+
+if (tipoDeSuscripcion == 'Basic') {
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+}
+else if (tipoDeSuscripcion == 'Free') {
+    console.log("Solo puedes tomar los cursos gratis");
+}
+else if (tipoDeSuscripcion == 'Expert'){
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+}
+else if (tipoDeSuscripcion == 'ExpertPlus'){
+    console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+}
